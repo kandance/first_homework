615964e4fa607cbcc68a7e08bcdd3712ba61dca6
Kandance Ferguson <kandance.ferguson@gmail.com>
2024-07-25 12:46:22 -0400

    first commit started two files based on objects and object and loops

---
+++ objectLoops.js
@@ -0,0 +1,100 @@
+let numbers = { a: 1, b: 2, c: 3 };
+
+let friends = {
+  best: 'Joan',
+  secondBest: 'Mari',
+  thirdBest: 'Elaine',
+  //Function in an object? this is called a method
+  listingFriends() {
+    for (let friend in friends) {
+      console.log(`friend at ${friend}: `, friends[friend]);
+    }
+  },
+};
+
+// console.log(friends.listingFriends());
+
+// for (let giggle in numbers) {
+//   console.log(`number at ${giggle}: `, numbers[giggle]);
+// }
+
+let people = [
+  { name: 'Michelle', city: 'Brooklyn' },
+  { name: 'Liz', city: 'Chicago' },
+  { name: 'Natalie', city: 'Los Angeles' },
+  { name: 'Katie', city: 'San Francisco' },
+  { name: 'Suzy', city: 'Panama' },
+  { name: 'Lacy', city: 'Jacksonville' },
+];
+
+for (let person in people) {
+  console.log(`${people[person].name} lives in ${people[person].city}`);
+}
+
+people.map((person, i) => {
+  console.log(
+    `This person is at index ${i} and ${person.name} lives in ${person.city}`
+  );
+});
+
+let item = [
+  {
+    id: '0001',
+    type: 'donut',
+    name: 'Cake',
+    ppu: 0.55,
+    batters: {
+      batter: [
+        { id: '1001', type: 'Regular' },
+        { id: '1002', type: 'Chocolate' },
+        { id: '1003', type: 'Blueberry' },
+        { id: '1004', type: "Devil's Food" },
+      ],
+    },
+    topping: [
+      { id: '5001', type: 'None' },
+      { id: '5002', type: 'Glazed' },
+      { id: '5005', type: 'Sugar' },
+      { id: '5007', type: 'Powdered Sugar' },
+      { id: '5006', type: 'Chocolate with Sprinkles' },
+      { id: '5003', type: 'Chocolate' },
+      { id: '5004', type: 'Maple' },
+    ],
+  },
+  {
+    id: '0002',
+    type: 'donut',
+    name: 'Raised',
+    ppu: 0.55,
+    batters: {
+      batter: [{ id: '1001', type: 'Regular' }],
+    },
+    topping: [
+      { id: '5001', type: 'None' },
+      { id: '5002', type: 'Glazed' },
+      { id: '5005', type: 'Sugar' },
+      { id: '5003', type: 'Chocolate' },
+      { id: '5004', type: 'Maple' },
+    ],
+  },
+  {
+    id: '0003',
+    type: 'donut',
+    name: 'Old Fashioned',
+    ppu: 0.55,
+    batters: {
+      batter: [
+        { id: '1001', type: 'Regular' },
+        { id: '1002', type: 'Chocolate' },
+      ],
+    },
+    topping: [
+      { id: '5001', type: 'None' },
+      { id: '5002', type: 'Glazed' },
+      { id: '5003', type: 'Chocolate' },
+      { id: '5004', type: 'Maple' },
+    ],
+  },
+];
+
+// console.log(item);
---
+++ objects.js
@@ -0,0 +1,50 @@
+const person = {
+  fname: 'Michelle',
+  lname: 'Schneider',
+  city: 'Brooklyn',
+  state: 'New York',
+};
+
+//Dot notation
+// console.log('city: ', person.city);
+
+//Bracket notation
+// console.log('city: ', person['city']);
+
+let state = 'state';
+// console.log('city: ', person[state]);
+
+const nestedPerson = {
+  name: {
+    first: 'Michelle',
+    last: 'Schneider',
+  },
+  city: 'Brooklyn',
+  state: 'New York',
+  favorites: {
+    foods: ['pizza', 'tacos', 'sushi', 'ramen'],
+    drinks: {
+      caffeinated: 'coffee',
+      decaffeinated: 'juice',
+      milks: {
+        cow: 'whole',
+        plant: 'almond',
+      },
+    },
+  },
+};
+
+//console.log('nested person: ', nestedPerson);
+console.log(`nested person's last name: `, nestedPerson.name.last);
+console.log(
+  `Michelle's favorite plant milk: `,
+  nestedPerson.favorites.drinks.milks.plant
+);
+
+function getFavoriteFoods(person) {
+  return person.favorites.foods.map((food) =>
+    console.log(`${person.name.first}'s favorite food is ${food}`)
+  );
+}
+
+getFavoriteFoods(nestedPerson);
